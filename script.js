d2168ae176ed37bd7375ac4b2bf2db1d377015d8
ethanschwartz225 <ethans@bu.edu>
2021-10-21 11:28:05 +0300

    update with hackathon1 files

--- script.js
+++ script.js
@@ -1,73 +1,12 @@
-// Extensive List of ingredients;
-applejuice = {
-    color:"yellow",
-    matter:"liquid",
-}
+let input = document.getElementById("input");
+let button = document.getElementById("button");
+let cocktailContainer = document.getElementById("cocktailContainer");
 
-orangejuice = {
-    color:"orange",
-    matter: "liquid",
-}
+button.addEventListener("click", addToList);
 
-ice = {
-    color:"lightgray",
-    matter:"solid",
-}
-
-vodka = {
-    color:"white",
-    matter:"liquid",
-}
-
-whiskey = {
-    color:"brown",
-    matter:"liquid",
-}
-
-tequila = {
-    color:"white",
-    matter:"liquid",
-}
-
-coke = {
-    color:"brown",
-    matter:"liquid",
-}
-
-lime = {
-    color:"green",
-    matter:"solid",
-}
-
-orange = {
-    color:"orange",
-    matter:"solid",
-}
-
-redbull = {
-    color:"gold",
-    matter:"liquid",
-}
-
-tomatojuice = {
-    color:"red",
-    matter:"liquid"
-}
-
-
-let enterButton = document.getElementById("enter");
-let newIngredient = document.createElement("p");
-let ingredient = document.getElementById("ingredient");
-let myCocktail = document.getElementById("myCocktail");
-
-enterButton.addEventListener("click", enterIngredient);
-
-function enterIngredient(event){
+function addToList(event) {
     event.preventDefault();
-    if(ingredient.value.length > 0) {
-        newIngredient.textContent = ingredient.value;
-        myCocktail.appendChild(newIngredient);
-        ingredient.value = "";
-    }
+    let newIngredient =input.value;
+    cocktailContainer.appendChild(newIngredient);
 }
 
