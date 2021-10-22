8560c247cf75a226c9d16a97a1de5dc8370d2f58
jeremy reiner <reiner_jeremy@yahoo.com>
2021-10-22 14:18:45 +0300

    added cup animation and fixed array on bottom of js in notes

--- hackathon/script.js
+++ hackathon/script.js
@@ -1,120 +1,163 @@
 // Extensive List of ingredients;
-let drink = [
-   
-    {
-    item: "Applejuice",
+let drink = {
+    liquid: [{
+    item: "apple juice",
     color:"yellow",
-    matter:"liquid",
     },
    
     {
-    item: "Orangeuice",
+    item: "orange juice",
     color:"orange",
-    matter: "liquid",
     },
-   
-    {
-    item: "ice",
-    color:"lightgray",
-    matter:"solid",
-    },
-   
     {
     item: "vodka",
     color:"white",
-    matter:"liquid",
      },
     
     {
     item: "whiskey",    
     color:"brown",
-    matter:"liquid",
     },
     
     {
     item: "tequila",
     color:"white",
-    matter:"liquid",
     },
     
     {
     item: "coke",
     color:"brown",
-    matter:"liquid",
+    },
+    {
+    item: "redbull",
+    color:"gold",
+    },
+    {
+    item: "tomato juice",
+    color:"red",
+    }
+    ],
+    solid: [{
+    item: "ice",
+    color:"lightgray",
     },
    
     {
     item: "lime",
     color:"green",
-    matter:"solid",
     },
    
     {
     item: "orange",
     color:"orange",
-    matter:"solid",
-    },
-   
-    {
-    item: "redbull",
-    color:"gold",
-    matter:"liquid",
-    },
-    {
-    item: "tomatojuice",
-    color:"red",
-    matter:"liquid"
-    }
-]
+    }]
+}
 let button = document.getElementById("button");
 let ingredientList = document.getElementById("ingredientList");
 let deleteButton = document.createElement("input");
 let addButton = document.createElement("input");
 let input = document.getElementById("input");
 
-function findColor(){
-for(i=0; i <drink.length; i++){
-    let drinkItem = drink[i];
-    let colorItem = drinkItem.color;
-    console.log(colorItem);
-    console.log(drinkItem);
-    }
-}
-findColor()
 
-// button.addEventListener("click", addToList);
+let colorItem = Object.keys(drink.liquid);
+console.log(colorItem)
+let drinkItem = Object.keys(drink.liquid[1]);
+// let CIValue = Object.value(colorItem)
+// console.log(drinkItem)
+
+
+// selcects object item inside of list of arrays. Need to figure out how to select an inputted item 
+// and automatically recieve its color
+// function findColor(){
+// for(i=0; i <drink.liquid.length; i++){
+//     if(input.value == drinkItem){
+//         // console.log(drinkItem)
+//     }
+//     else if(input.value = colorItem){
+//         console.log("we dont have this drink")
+//     }
+//     else{
+//         console.log("couldnt match a color")
+//     }
+//     }   
+// }
+// findColor()
+
+
+
+button.addEventListener("click", addToList);
 // button.addEventListener("click", addToCup);
-// function addToList(event) {
-//     event.preventDefault()
-//     let newIngredient = document.createElement("h3");
-//     // drinkColor(input.value)
+function addToList(event) {
+    event.preventDefault()
+    let newIngredient = document.createElement("h3");
+    newIngredient.append(input.value);
+    ingredientList.appendChild(newIngredient);
+    input.value = "";
+    // let liquidObj = drink.liquid.input.value;
+    // console.log(liquidObj)
 
+    //creates button to add an item to recipe list again
+    newIngredient.appendChild(addButton);
+        addButton.setAttribute("type", "button");
+        addButton.setAttribute("value", "Add");
+        addButton.classList.add("addButton");
 
-//     //need to find the color when the object user inputs the object item
-//     // console.log(drink.find(input.value));
-//     newIngredient.append(input.value);
-//     // ingredientList.appendChild(newIngredient);
-//     input.value = "";
+    //  creates button to omit an item from recipe list
+    newIngredient.appendChild(deleteButton);
+         deleteButton.setAttribute("type", "button");
+        deleteButton.setAttribute("value", "Delete");
+        deleteButton.classList.add("deleteButton");
+        deleteButton.addEventListener("click", function(event){
+        newIngredient.remove()
+        })
+        ingredientList.appendChild(newIngredient);
+}
 
-//     //  creates button to add an item to recipe list again
-//     newIngredient.appendChild(addButton);
-//         addButton.setAttribute("type", "button");
-//         addButton.setAttribute("value", "Add");
-//         addButton.classList.add("addButton");
+// function addToCup(event){
+//     event.preventDefault();
 
-//     //  creates button to omit an item from recipe list
-//     newIngredient.appendChild(deleteButton);
-//          deleteButton.setAttribute("type", "button");
-//         deleteButton.setAttribute("value", "Delete");
-//         deleteButton.classList.add("deleteButton");
-//         deleteButton.addEventListener("click", function(event){
-//         newIngredient.remove()
-//         })
-//         ingredientList.appendChild(newIngredient);
 // }
 
-// // function addToCup(event){
-// //     event.preventDefault();
 
-// // }
+
+
+
+
+
+// let bgColor 
+// function getBgColor(){
+//    colorBg =  this.style.backgroundColor; 
+// }
+// let cocktails =  {drinks: [
+//     {color: "orange", drink: "orange juice"},
+//     {color: "yellow", drink: "apple juice"},
+//     {color: "white", drink: "milk"},
+//     {color: "brown", drink: "coffee"},
+//     {color: "gold", drink: "red bull"},
+//     {color: "blue", drink: "water"},
+//     {color: "red", drink: "tomato juice"},
+//     {color: "purple", drink: "prune juice"}
+// ]
+// }
+// let button = document.getElementById("button");
+// let ingredientList = document.getElementById("ingredientList");
+// let deleteButton = document.createElement("input");
+// let addButton = document.createElement("input");
+// let input = document.getElementById("input");
+// let ul = document.createElement("ul")
+
+// let drinkinput = cocktails.drinks
+// let objKey = Object.keys(drinkinput)
+// let objValue = Object.values(drinkinput)
+// for (i=0; i < drinkinput.length; i++){
+//     ul.classList.add("listOfDrinks")
+//     let li = document.createElement("li")
+//     li.setAttribute("id", drinkinput[i].color)
+//     li.innerText = drinkinput[i].drink
+//     li.style.border = "1px solid black";
+//     li.classList.add("drinkColor");
+//     ul.appendChild(li)
+//     ingredientList.appendChild(ul)
+// }
+
 
